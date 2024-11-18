56dfb0b3ea368b97ff1a2536a7b66b7039f846b0
Adiel Rodriguez <adiel.rodriguez39@myhunter.cuny.edu>
2024-11-18 03:02:02 -0500

    update register

--- src/components/Register.js
+++ src/components/Register.js
@@ -93,133 +93,120 @@ function Register() {
 
     return (
         <div className="register bg-body-tertiary">
-            <section className="vh-100">
+          <section className="vh-100">
             <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-lg-12 col-xl-11">
-                    <div
+              <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-lg-6 col-md-8">
+                  <div
                     className="card"
                     style={{ borderRadius: "25px" }}
-                    >
+                  >
                     <div className="card-body p-md-5">
-                        <div className="row justify-content-center">
-                        <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                            Sign up
-                            </p>
-                            <form
-                            className="mx-1 mx-md-4"
-                            onSubmit={(e) => submitRegistration(e)}
-                            >
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <div className="form-outline flex-fill mb-0">
-                                <input
-                                    type="text"
-                                    name="Username"
-                                    id="form3Example1c"
-                                    className="form-control"
-                                    placeholder="Username"
-                                    value={username}
-                                    onChange={handleUsernameChange}
-                                />
-                                </div>
-                            </div>
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <div className="form-outline flex-fill mb-0">
-                                <input
-                                    type="tel"
-                                    name="Phone Number (optional)"
-                                    id="form3Example1c"
-                                    className="form-control"
-                                    placeholder="Phone Number (optional)"
-                                    value={phone_number}
-                                    onChange={handlePhoneNumberChange}
-                                />
-                                </div>
-                            </div>
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <div className="form-outline flex-fill mb-0">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="form3Example3c"
-                                    className="form-control"
-                                    placeholder="Email"
-                                    value={email}
-                                    onChange={handleEmailChange}
-                                />
-                                </div>
-                            </div>
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <div className="form-outline flex-fill mb-0">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="form3Example4c"
-                                    className="form-control"
-                                    placeholder="password"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                                </div>
-                            </div>
-                            <div className="d-flex flex-row align-items-center mb-4">
-                                <div className="form-outline flex-fill mb-0">
-                                <input
-                                    type="password"
-                                    name="repeatPassword"
-                                    id="form3Example4cd"
-                                    className="form-control"
-                                    placeholder="repeat password"
-                                    value={repeatPassword}
-                                    onChange={handleRepeatPassword}
-                                />
-                                </div>
-                            </div>
-                            <div className="form-check d-flex justify-content-center mb-5">
-                                <input
-                                className="form-check-input me-2"
-                                type="checkbox"
-                                value={agreeToTerms}
-                                id="form2Example3c"
-                                onChange={handleAgreeToTerms}
-                                />
-                                <label
-                                className="form-check-label"
-                                htmlFor="form2Example3"
-                                style={{ color:"white"}}
-                                >
-                                I agree all statements in{" "}
-                                <a href="#!">Terms of service</a>
-                                </label>
-                            </div>
-                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                <button
-                                type="submit"
-                                className="btn btn-primary btn-lg"
-                                >
-                                Register
-                                </button>
-                            </div>
-                            </form>
-                        </div>
-                        <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                            <img
-                            src="/nyc.jpeg"
-                            className="img-fluid"
-                            alt="Sample"
-                            style={{width: "500px",height: "300px"}}
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Sign up
+                      </p>
+                      <form
+                        className="mx-1 mx-md-4"
+                        onSubmit={(e) => submitRegistration(e)}
+                      >
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="text"
+                              name="Username"
+                              id="form3Example1c"
+                              className="form-control"
+                              placeholder="Username"
+                              value={username}
+                              onChange={handleUsernameChange}
                             />
+                          </div>
                         </div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="tel"
+                              name="Phone Number (optional)"
+                              id="form3Example1c"
+                              className="form-control"
+                              placeholder="Phone Number (optional)"
+                              value={phone_number}
+                              onChange={handlePhoneNumberChange}
+                            />
+                          </div>
                         </div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              name="email"
+                              id="form3Example3c"
+                              className="form-control"
+                              placeholder="Email"
+                              value={email}
+                              onChange={handleEmailChange}
+                            />
+                          </div>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              name="password"
+                              id="form3Example4c"
+                              className="form-control"
+                              placeholder="Password"
+                              value={password}
+                              onChange={handlePasswordChange}
+                            />
+                          </div>
+                        </div>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              name="repeatPassword"
+                              id="form3Example4cd"
+                              className="form-control"
+                              placeholder="Repeat Password"
+                              value={repeatPassword}
+                              onChange={handleRepeatPassword}
+                            />
+                          </div>
+                        </div>
+                        <div className="form-check d-flex justify-content-center mb-5">
+                          <input
+                            className="form-check-input me-2"
+                            type="checkbox"
+                            value={agreeToTerms}
+                            id="form2Example3c"
+                            onChange={handleAgreeToTerms}
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="form2Example3"
+                          >
+                            I agree to all statements in{" "}
+                            <a href="#!">Terms of Service</a>
+                          </label>
+                        </div>
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="submit"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Register
+                          </button>
+                        </div>
+                      </form>
                     </div>
-                    </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </section>
-      </div>
-    )
+          </section>
+        </div>
+      );
 }
 
 export default Register;
